7dc176ad1388d369a67f84e9c40813e6d15a0c66
royjh <zithlos@gmail.com>
2022-05-01 18:16:34 -0500

    adds numbers to calculate the sums of the multiples

--- multiples.py
+++ multiples.py
@@ -1,12 +1,18 @@
 #Prints numbers, up to and including 1000, that are multiples of 3 or 5.
 count = 0
-while count <= 1000:
+count_sum = 0
+while count < 1000:
     if count % 3 == 0 and count % 5 == 0:
         print (str(count) + " Double Trouble")
+        count_sum += count
 
     elif count % 3 == 0:
         print (count)
+        count_sum += count
 
     elif count % 5 == 0:
         print (count)
+        count_sum += count
     count += 1
+
+print (count_sum)
